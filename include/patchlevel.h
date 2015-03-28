abdaace3369ddce80b59247f03a22fdb6c94d8a4
Soviet5lo <Soviet5lodev@gmail.com>
2015-03-28 10:55:35 -0500

    New versioning scheme.
    Prep for server build + project rename.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -13,16 +13,14 @@
 /*#define DEF_GAME_NAME   "NetHack"*/
 
 /* Version */
-/* edit by Amy - version_minor is updated for each release, version_major increases by 1 if minor reaches 100 */
 #define VERSION_MAJOR   0
-#define VERSION_MINOR   75
+#define VERSION_MINOR   4
 /*
  * PATCHLEVEL is updated for each release.
- * edit by Amy - I'm keeping patch, edit and fixlevel to show the game's based on SLASH'EM 0.0.7E7F2.
  */
-#define PATCHLEVEL      7
-#define EDITLEVEL	7
-#define FIXLEVEL        2
+#define PATCHLEVEL      0
+#define EDITLEVEL	0
+/*#define FIXLEVEL        0*/
 
 #define COPYRIGHT_BANNER_A \
 "This is SuperLotsoAddedStuffHack-Extended Magic 1997-2005"
@@ -46,7 +44,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00000702L
+#define VERSION_COMPATIBILITY 0x00040000L
 #endif
 
 /*patchlevel.h*/
