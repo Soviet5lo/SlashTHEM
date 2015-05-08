e4a77f159b11e03a692c0b50a8613279ebdf3014
Soviet5lo <Soviet5lodev@gmail.com>
2015-05-08 18:46:23 -0500

    Whoops.  Fix a build error.

--- src/monmove.c
+++ src/monmove.c
@@ -291,8 +291,9 @@ boolean fleemsg;
 		if (fleetime == 1) fleetime++;
 		mtmp->mfleetim = min(fleetime, 127);
 	    }
-	    if (!mtmp->mflee && fleemsg && canseemon(mtmp) && !mtmp->mfrozen) {
+	    if (!mtmp->mflee && fleemsg && canseemon(mtmp) && !mtmp->mfrozen)
 #if 0 /* 5lo: Reverted (for now?) */
+ {
 		if (rn2(3)) {
 		  pline("%s turns to flee!", (Monnam(mtmp)));
 		  mtmp->mflee = 1;
