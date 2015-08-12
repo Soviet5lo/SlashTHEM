d60ece60b06be45e59cb54ad0b896a3e1191669b
Soviet5lo <Soviet5lodev@gmail.com>
2015-08-12 18:49:02 -0500

    Show max skill levels in #enhance menu.
    Players should no longer have to make a guess or sourcedive in order to
    see a max level of a skill.
    
    Originally concieved in Slash'EM Extended, and implemented in dNethack
    and Nethack Fourk.

--- src/weapon.c
+++ src/weapon.c
@@ -136,6 +136,7 @@ STATIC_DCL boolean FDECL(can_practice, (int)); /* WAC for Practicing */
 #ifdef OVL1
 
 STATIC_DCL char *FDECL(skill_level_name, (int,char *));
+STATIC_DCL char *FDECL(max_skill_level_name, (int,char *));
 STATIC_DCL void FDECL(skill_advance, (int));
 
 #endif	/* OVL1 */
@@ -1031,6 +1032,28 @@ char *buf;
     return buf;
 }
 
+/* copy the skill level name into the given buffer */
+STATIC_OVL char *
+max_skill_level_name(skill, buf)
+int skill;
+char *buf;
+{
+    const char *ptr;
+
+    switch (P_MAX_SKILL(skill)) {
+	case P_UNSKILLED:    ptr = "Unskilled"; break;
+	case P_BASIC:	     ptr = "Basic";     break;
+	case P_SKILLED:	     ptr = "Skilled";   break;
+	case P_EXPERT:	     ptr = "Expert";    break;
+	/* these are for unarmed combat/martial arts only */
+	case P_MASTER:	     ptr = "Master";    break;
+	case P_GRAND_MASTER: ptr = "Grand Master"; break;
+	default:	     ptr = "Unknown";	break;
+    }
+    Strcpy(buf, ptr);
+    return buf;
+}
+
 /* return the # of slots required to advance the skill */
 STATIC_OVL int
 slots_required(skill)
@@ -1534,7 +1557,7 @@ int enhance_skill(boolean want_dump)
 {
     int pass, i, n, len, longest,
 	to_advance, eventually_advance, maxxed_cnt;
-    char buf[BUFSZ], sklnambuf[BUFSZ];
+    char buf[BUFSZ], sklnambuf[BUFSZ], maxsklnambuf[BUFSZ];
     const char *prefix;
     menu_item *selected;
     anything any;
@@ -1656,6 +1679,7 @@ int enhance_skill(boolean want_dump)
 		    prefix = (to_advance + eventually_advance +
 				maxxed_cnt > 0) ? "    " : "";
 		(void) skill_level_name(i, sklnambuf);
+		(void) max_skill_level_name(i, maxsklnambuf);
 #ifdef WIZARD
 		if (wizard) {
 		    if (!iflags.menu_tab_sep)
@@ -1672,11 +1696,11 @@ int enhance_skill(boolean want_dump)
 #endif
 		{
 		    if (!iflags.menu_tab_sep)
-			Sprintf(buf, " %s %-*s [%s]",
-			    prefix, longest, P_NAME(i), sklnambuf);
+			Sprintf(buf, " %s %-*s [%s / %s]",
+			    prefix, longest, P_NAME(i), sklnambuf,maxsklnambuf);
 		    else
-			Sprintf(buf, " %s%s\t[%s]",
-			    prefix, P_NAME(i), sklnambuf);
+			Sprintf(buf, " %s%s\t[%s / %s]",
+			    prefix, P_NAME(i), sklnambuf,maxsklnambuf);
 		}
 		any.a_int = can_advance(i, speedy) ? i+1 : 0;
 		add_menu(win, NO_GLYPH, &any, 0, 0, ATR_NONE,
