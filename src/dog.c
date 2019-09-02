3849c501d2786e5572a0cf5ddb6e98698ef67d83
Soviet5lo <Soviet5lodev@gmail.com>
2019-09-02 13:11:56 -0500

    Remove a leftover monster from Rocker removal.

--- src/dog.c
+++ src/dog.c
@@ -266,7 +266,6 @@ makedog()
     }
 	if (pettype == PM_MONKEY) petname = "Ugga-Ugga";
 	if (pettype == PM_PARROT) petname = "Squawks";
-	if (pettype == PM_SPEEDHORSE) petname = "Harley Davidson";
 	if (pettype == PM_BABY_CROCODILE) petname = "Snappy"; /* in Germany it would be called Schnappi */
 
 	if (pettype == PM_GREEN_ELF) petname = "Dray Harp";
@@ -286,7 +285,7 @@ makedog()
 
 #ifdef STEED
 	/* Horses already wear a saddle */
-	if ((pettype == PM_PONY || pettype == PM_SPEEDHORSE) && !!(otmp = mksobj(SADDLE, TRUE, FALSE))) {
+	if (pettype == PM_PONY && !!(otmp = mksobj(SADDLE, TRUE, FALSE))) {
 	    if (mpickobj(mtmp, otmp))
 		panic("merged saddle?");
 	    mtmp->misc_worn_check |= W_SADDLE;
