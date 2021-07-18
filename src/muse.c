dfc4c5a3b5a7e70712e00e3eb7147a3428565389
Soviet5lo <Soviet5lodev@gmail.com>
2021-07-18 00:05:00 -0500

    Fix use after free from monsters reading/quaffing
    hdf commit d899779

--- src/muse.c
+++ src/muse.c
@@ -1869,9 +1869,10 @@ struct monst *mtmp;
 	    	    	}
 		    }
 		}
+		boolean wascursed = otmp->cursed;
 		m_useup(mtmp, otmp);
 		/* Attack the player */
-		if (distmin(mmx, mmy, u.ux, u.uy) == 1 && !otmp->cursed) {
+		if (distmin(mmx, mmy, u.ux, u.uy) == 1 && !wascursed) {
 		    int dmg;
 		    struct obj *otmp2;
 
@@ -2843,7 +2844,6 @@ boolean stoning;
 	obj->quan = save_quan;
     } else if (flags.soundok)
 	You_hear("%s.", (obj->otyp == POT_ACID) ? "drinking" : "chewing");
-    m_useup(mon, obj);
     if (((obj->otyp == POT_ACID) || acidic(&mons[obj->corpsenm])) &&
 		    !resists_acid(mon)) {
 	mon->mhp -= rnd(15);
@@ -2851,6 +2851,7 @@ boolean stoning;
 	    Monnam(mon));
     }
     if (mon->mhp <= 0) {
+	m_useup(mon, obj);
 	pline("%s dies!", Monnam(mon));
 	if (by_you) xkilled(mon, 0);
 	else mondead(mon);
@@ -2875,6 +2876,7 @@ boolean stoning;
 	edog->hungrytime += nutrit;
 	mon->mconf = 0;
     }
+    m_useup(mon, obj);
     mon->mlstmv = monstermoves; /* it takes a turn */
 }
 
